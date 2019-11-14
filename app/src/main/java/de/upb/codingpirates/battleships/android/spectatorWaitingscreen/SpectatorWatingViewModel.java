3cbe457c0bd972df61f48e1ca97bd45b6411a6ba
kroegerl <kroegerl@mail.upb.de>
2019-11-14 12:55:39 +0100

    SpectatorWatingViewModel added

---
+++ app/src/main/java/de/upb/codingpirates/battleships/android/spectatorWaitingscreen/SpectatorWatingViewModel.java
@@ -0,0 +1,6 @@
+package de.upb.codingpirates.battleships.android.spectatorWaitingscreen;
+
+import androidx.databinding.BaseObservable;
+
+public class SpectatorWatingViewModel extends BaseObservable {
+}
