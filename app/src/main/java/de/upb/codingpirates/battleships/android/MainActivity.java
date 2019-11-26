8f5b64396d5e7633abf229a7c2aab987cca8197e
Fynn R <fynrup@mail.upb.de>
2019-11-26 11:55:18 +0100

    Added picture of leaderboard

--- app/src/main/java/de/upb/codingpirates/battleships/android/MainActivity.java
+++ app/src/main/java/de/upb/codingpirates/battleships/android/MainActivity.java
@@ -1,6 +1,7 @@
 package de.upb.codingpirates.battleships.android;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
