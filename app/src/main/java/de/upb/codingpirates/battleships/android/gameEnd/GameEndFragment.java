eb3bcb362c170ee6bad1af38d0f23cc35ce461a4
fynrup <fynrup@mail.uni-paderborn.de>
2019-11-27 19:39:17 +0100

    The ranking-table in the end can now be filled dynamically via fillTable()

--- app/src/main/java/de/upb/codingpirates/battleships/android/gameEnd/GameEndFragment.java
+++ app/src/main/java/de/upb/codingpirates/battleships/android/gameEnd/GameEndFragment.java
@@ -1,10 +1,15 @@
 package de.upb.codingpirates.battleships.android.gameEnd;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
@@ -12,13 +17,77 @@ import de.upb.codingpirates.battleships.android.R;
 import de.upb.codingpirates.battleships.android.databinding.GameendFragmentBinding;
 
 public class GameEndFragment extends Fragment {
+
+    private View view;
+    TableLayout layout;
+
+    /**
+     * //TODO Beschreibung
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         GameendFragmentBinding databinding = DataBindingUtil.inflate(inflater, R.layout.gameend_fragment,container,false);
         databinding.setViewmodel(new GameEndViewModel());
-
-
+        view = databinding.getRoot();
+        layout = view.findViewById(R.id.playerRankingTableLayout);
+        fillTable(5);
         return databinding.getRoot();
+    }
+
+    /**
+     * author Fynn Ruppel
+     * //TODO Beschreibung
+     * @param amount amount of rows that need to be added to the table
+     */
+    private void fillTable(int amount) {
+        for(int i = 0; i< amount; i++) {
+            addRow(i+1,"Peter",300-i+10,i+1);
+        }
+    }
+
+    /**
+     * author : Fynn Ruppel
+     * This method adds one row to the EndScreen Player ranking table. The params define the attributes of the player added to the table
+     * @param p the place in which the player is ranked
+     * @param n name of the player being added
+     * @param s score of the player being added
+     * @param id id of the row
+     */
+    private void addRow(int p, String n, int s, int id) {
+        //create a new row and define the layout style
+        TableRow row = new TableRow(this.getContext());
+        TableRow.LayoutParams rowParams = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT);
+        TableRow.LayoutParams textViewParams = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 1f);
+
+        //create the 3 textViews and add the needed layout styles to be displayed correctly
+        row.setLayoutParams(rowParams);
+       TextView place = new TextView(this.getContext());
+       place.setLayoutParams(textViewParams);
+       place.setGravity(Gravity.CENTER);
+       TextView name = new TextView(this.getContext());
+        name.setLayoutParams(textViewParams);
+        name.setGravity(Gravity.CENTER);
+       TextView score = new TextView(this.getContext());
+        score.setLayoutParams(textViewParams);
+        score.setGravity(Gravity.CENTER);
+       /*place.setId(id+1);
+       name.setId(id+2);
+       score.setId(id+3);*/
+       place.setText("" + p);
+       name.setText(n);
+       score.setText("" + s);
+
+       //add the textViews accordingly to the created row
+       row.addView(place, 0);
+       row.addView(name,1);
+       row.addView(score,2);
+
+       //add the new row to the existing tableLayout
+       layout.addView(row, rowParams);
     }
 }
