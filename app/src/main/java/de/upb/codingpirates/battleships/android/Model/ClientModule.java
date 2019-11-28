22760d84eeaaa1827fa533f61fef3f52d1526921
Paul Becker <paube@mail.uni-paderborn.de>
2019-11-28 18:01:19 +0100

    client Connection

--- app/src/main/java/de/upb/codingpirates/battleships/android/Model/ClientModule.java
+++ app/src/main/java/de/upb/codingpirates/battleships/android/Model/ClientModule.java
@@ -5,19 +5,15 @@ import com.google.inject.Singleton;
 
 import de.upb.codingpirates.battleships.client.Handler;
 import de.upb.codingpirates.battleships.client.network.AbstractClientModule;
-import de.upb.codingpirates.battleships.client.network.ClientConnector;
-import de.upb.codingpirates.battleships.network.ConnectionHandler;
-import de.upb.codingpirates.battleships.network.network.module.ClientNetworkModule;
 
-public class ClientModule extends AbstractClientModule {
+public class ClientModule extends AbstractClientModule<ClientConnector> {
     public ClientModule() {
         super(ClientConnector.class);
     }
     @Override
     protected void configure() {
         super.configure();
-        //this.install(new ClientNetworkModule());
-        //this.bind(ConnectionHandler.class).to(ClientConnector.class).in(Singleton.class);
+
         this.bind(Handler.class).toInstance(new MessageHandler());
     }
 }
