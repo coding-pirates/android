321a3d214250a950608f86e59ea39d63773631f8
fynrup <fynrup@mail.uni-paderborn.de>
2019-12-01 17:32:56 +0100

    Added ids to the input layouts in order to display error messages in the login fragment

--- app/src/main/java/de/upb/codingpirates/battleships/android/login/LoginViewModel.java
+++ app/src/main/java/de/upb/codingpirates/battleships/android/login/LoginViewModel.java
@@ -6,27 +6,45 @@ import androidx.lifecycle.ViewModel;
 import androidx.navigation.Navigation;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import de.upb.codingpirates.battleships.android.R;
 import de.upb.codingpirates.battleships.android.Model.Model;
 
+/**
+ * @author Lukas Kröger, Fynn Ruppel
+ */
 public class LoginViewModel extends ViewModel {
+
     private Model model = Model.getInstance();
+
     private TextInputEditText usernameField;
     private TextInputEditText serverIpField;
     private TextInputEditText serverPortField;
 
+    private TextInputLayout usernameLayout;
+    private TextInputLayout serverIpLayout;
+    private TextInputLayout serverPortLayout;
+
+    /**
+     * TODO description
+     * @param view
+     */
     public void loginButtonClicked(View view){
         //get the current values of the input fields
         usernameField = view.getRootView().findViewById(R.id.usernameInputView);
         serverIpField = view.getRootView().findViewById(R.id.serverIpInputView);
         serverPortField = view.getRootView().findViewById(R.id.serverPortInputView);
-
         String currUsername = usernameField.getText().toString();
         String serverIP = serverIpField.getText().toString();
         String serverPort = serverPortField.getText().toString();
 
+        //get the insances of the input layouts
+        usernameLayout = view.getRootView().findViewById(R.id.usernameInputLayout);
+        serverIpLayout = view.getRootView().findViewById(R.id.serverIpInputLayout);
+        serverPortLayout = view.getRootView().findViewById(R.id.serverPortInputLayout);
+
         //TODO pass the username to the model and validate the username
-        Navigation.findNavController(view).navigate(R.id.action_loginFragment_to_lobbyFragment);
+        //Navigation.findNavController(view).navigate(R.id.action_loginFragment_to_lobbyFragment);
     }
 }
