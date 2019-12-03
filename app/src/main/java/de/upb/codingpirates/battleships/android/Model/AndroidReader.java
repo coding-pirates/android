932c3ff967a6f404d07fa2bfe2ca43ea9f85cff9
Paul Becker <paube@mail.uni-paderborn.de>
2019-12-03 20:51:52 +0100

    Fix clientMessageDispatcher being disposed

---
+++ app/src/main/java/de/upb/codingpirates/battleships/android/Model/AndroidReader.java
@@ -0,0 +1,39 @@
+package de.upb.codingpirates.battleships.android.Model;
+
+import java.io.IOException;
+import java.net.SocketException;
+
+import de.upb.codingpirates.battleships.logic.util.Pair;
+import de.upb.codingpirates.battleships.network.Connection;
+import de.upb.codingpirates.battleships.network.exceptions.parser.ParserException;
+import de.upb.codingpirates.battleships.network.message.Message;
+import de.upb.codingpirates.battleships.network.message.report.ConnectionClosedReport;
+import de.upb.codingpirates.battleships.network.util.ClientReaderMethod;
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+
+public class AndroidReader implements ClientReaderMethod {
+
+    @Override
+    public void get(Connection connection, Consumer<Pair<Connection, Message>> dispatch, Consumer<Throwable> error) {
+        Observable.create((ObservableEmitter<Pair<Connection, Message>> emitter) -> {
+            while (!connection.isClosed()) {
+                try {
+                    Message message = connection.read();
+                    emitter.onNext(new Pair<>(connection, message));
+                    break;
+                } catch (SocketException e) {
+                    connection.close();
+                    emitter.onNext(new Pair<>(connection, new ConnectionClosedReport()));
+                } catch (IOException | ParserException e) {
+                    emitter.onError(e);
+                }
+
+            }
+            emitter.onComplete();
+        }).observeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).doOnDispose(()->get(connection,dispatch,error)).doOnComplete(()->get(connection,dispatch,error)).subscribe(dispatch, error);
+    }
+}
