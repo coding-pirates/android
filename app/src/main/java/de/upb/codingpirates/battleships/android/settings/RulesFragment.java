05daa6fa3e8fa6f5dad81f83805019d237d320e8
fynrup <fynrup@mail.uni-paderborn.de>
2019-12-31 13:28:27 +0100

    Changed the width of the rules Dialog

--- app/src/main/java/de/upb/codingpirates/battleships/android/settings/RulesFragment.java
+++ app/src/main/java/de/upb/codingpirates/battleships/android/settings/RulesFragment.java
@@ -3,20 +3,39 @@ package de.upb.codingpirates.battleships.android.settings;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.fragment.app.DialogFragment;
 import androidx.navigation.Navigation;
 
 import de.upb.codingpirates.battleships.android.R;
 public class RulesFragment extends DialogFragment {
+
+        private int width = Resources.getSystem().getDisplayMetrics().widthPixels;
+
         @Override
         public Dialog onCreateDialog(Bundle savedInstanceState) {
             // Use the Builder class for convenient dialog construction
-            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(), R.style.MyCustomTheme);
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(), R.style.RulesDialogTheme);
             builder.setTitle(R.string.rulesStr)
                     .setMessage(R.string.spielregelnText);
             // Create the AlertDialog object and return it
             return builder.create();
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            Window window = getDialog().getWindow();
+            if(window == null) return;
+            WindowManager.LayoutParams params = window.getAttributes();
+            
+            //sets the width of the AlertDialog to 90% of Screen-Width
+            params.width = (int) (width * 0.9f);
+            window.setAttributes(params);
         }
     }
